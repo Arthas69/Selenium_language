5ebc915af6ebe611297a55723c5fec48a4fba768
Mkrtychyan Eduard <48879789+Arthas69@users.noreply.github.com>
2020-12-08 14:10:39 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,3 +1,5 @@
+import time
+
 import pytest
 
 from selenium.webdriver.support.ui import WebDriverWait
@@ -8,23 +10,12 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
 class TestLanguage:
-
-    def test_busket_button_text_on_espanol(self, browser):
+    def test_add_basket_button_exists(self, browser):
         browser.get(link)
+        # time.sleep(10)
         element = WebDriverWait(browser, 10).until(
             ec.visibility_of_element_located((By.CSS_SELECTOR, 'button.btn-add-to-basket'))
         )
-        element_text = element.text
+        button_classes = element.get_attribute('class')
 
-        assert element_text == 'Añadir al carrito', f"Should be 'Añadir al carrito' but actually got '{element_text}'"
-
-    # @pytest.mark.xfail
-    # @pytest.mark.skip
-    # def test_busket_button_text_on_francais(self, browser):
-    #     browser.get(link)
-    #     element = WebDriverWait(browser, 10).until(
-    #         ec.visibility_of_element_located((By.CSS_SELECTOR, 'button.btn-add-to-basket'))
-    #     )
-    #     element_text = element.text
-    #
-    #     assert element_text == 'Ajouter au panier', f"Should be 'Ajouter au panier' but actually got '{element_text}'"
+        assert 'btn-add-to-basket' in button_classes, 'Not the needed button'
